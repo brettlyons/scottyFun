5c2267850b38c416856ca9e8637631da524a3de6
Brett Lyons <blyons@students.naropa.edu>
2015-09-05 20:27:13 -0600

    Initial Commit

---
+++ Main.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty
+import System.Environment
+import Control.Monad
+
+main = do
+  port  <- liftM read $ getEnv "PORT"
+  scotty port $ do
+    get "/" $ do
+      html "Hello World!"
